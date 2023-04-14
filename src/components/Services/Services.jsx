502948d9a7af73c950ed7d746fe810a8f82a9b06
Nikita <nikita@Nikitas-MBP.attlocal.net>
2023-04-14 18:23:37 -0500

    first commit

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -14,6 +14,7 @@ import twitch from "../../img/socials/twitch.svg";
 export default function Services() {
   return (
     <div>
+      <ContactsBg />
       <div className="services-page-container">
         <Menu right={true} />
         <div className="services-content-container">
